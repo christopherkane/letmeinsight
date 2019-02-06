3e4941229ea93720a047dec8f9f9876b31c3fc3e
Christopher Kane <ckane@rapid7.com>
2019-02-06 18:38:00 +0000

    Adding employees

--- createEmployeeDb.py
+++ createEmployeeDb.py
@@ -2,12 +2,34 @@
 
 import sqlite3
 
-conn = sqlite3.connect('employees.db')
+USERS = [
+  ('Christopher Kane', 'christopher_kane@rapid7.com'),
+  ('Iain Wilson', 'iain_wilson@rapid7.com'),
+  ('Conor McAteer', 'conor_mcateer@rapid7.com'),
+  ('Connor Knox', 'connor_knox@rapid7.com'),
+  ('Scott Devlin', 'scott_devlin@rapid7.com'),
+  ('Nick Mifsud', 'nick_mifsud@rapid7.com'),
+  ('Arnaud Roger', 'arnaud_roger@rapid7.com'),
+  ('Chris Laughlin', 'chris_laughlin@rapid7.com')
+]
 
+INSERT = '''
+INSERT INTO employees(name,email)
+  VALUES(?,?)
+'''
+
+database = 'employees.db'
+
+# create a database connection
+conn = sqlite3.connect(database)
 print('Opened database successfully');
 
-conn.execute('CREATE TABLE employees (id INTEGER PRIMARY KEY, name TEXT, email TEXT, inOffice BOOLEAN)')
-
+conn.execute('CREATE TABLE employees (id INTEGER PRIMARY KEY, name TEXT, email TEXT, inOffice BOOLEAN DEFAULT false)')
 print('Table created successfully');
 
+for user in USERS:
+    print('Inserting: ' + user[0])
+    conn.execute(INSERT, user)
+
+conn.commit()
 conn.close()

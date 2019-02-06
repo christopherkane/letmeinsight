261a90ad0f5ba7403e532d430d539220d9628227
Conor McAteer <cmcateer@rapid7.com>
2019-02-06 19:36:18 +0000

    Added Flask API - definitley wrong but will test to see first

--- employeesDbScript.py
+++ employeesDbScript.py
@@ -1,19 +1,39 @@
 from flask import Flask
+from flask import request
 import sqlite3 as sql
 app = Flask(__name__)
 
 
 @app.route('/howmany',methods = ['POST', 'GET'])
+def howmany():
+	con = sql.connect("employees.db")
+	con.row_factory = sql.Row
 
+	cur = con.cursor();
+	cur.execute(COUNT(ALL,"select name from employees where inOffice = true")).fetchall()
+
+	con.commit()
+	con.close()
 
 @app.route('/list',methods = ['POST', 'GET'])
 def list():
-con = sql.connect("employees.db")
-   con.row_factory = sql.Row
+	con = sql.connect("employees.db")
+   	con.row_factory = sql.Row
 
-   cur = con.cursor()
-   cur.execute("select * from employees")
+   	cur = con.cursor()
+   	cur.execute("select * from employees")
 
-   rows = cur.fetchall();
+	con.commit()
+	con.close()
 
 @app.route('/isinoffice',methods = ['POST', 'GET'])
+def isinoffice():
+	user = request.args.get('user')
+	con = sql.connect("employees.db")
+   	con.row_factory = sql.Row
+
+   	cur = con.cursor()
+	cur.execute("select id from employees where name = ?", [user])
+	
+	con.commit()
+	con.close()

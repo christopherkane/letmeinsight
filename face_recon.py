58c0aaa0a287f91aadd5e4e008ad8eefa9b03b04
Christopher Kane <ckane@rapid7.com>
2019-02-06 19:57:57 +0000

    Adding multiple camers for entry/exit trakcing

--- face_recon.py
+++ face_recon.py
@@ -4,88 +4,98 @@ import cv2
 import sqlite3
 
 def main():
-    employee_dict = generateUsers()
+
+    database = 'employees.db'
+    conn = sqlite3.connect(database)
+
+    employee_dict = generateUsers(conn)
+
     # Get a reference to webcam #0 (the default one)
-    video_capture = cv2.VideoCapture(1)
+    video_capture_0 = cv2.VideoCapture(0)
+    video_capture_1 = cv2.VideoCapture(1)
     
     known_face_names = list()
     known_face_encodings = list()
     
     for filename in os.listdir('dataset'):
-        print(filename.split('.'))
         known_face_names.append(employee_dict[filename.split('.')[0]])
         known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file('dataset/' + filename))[0])
-    
+     
+    while True:
+        process(conn, known_face_names, known_face_encodings, video_capture_0, True, "Entry")
+        process(conn, known_face_names, known_face_encodings, video_capture_1, False, "Exit")
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+ 
+def process(conn, known_face_names, known_face_encodings, video_capture, setInOffice, identifier):
     # Initialize some variables
     face_locations = []
     face_encodings = []
     face_names = []
     process_this_frame = True
-    
-    while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-    
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-    
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-    
-                # If a match was found in known_face_encodings, just use the first one.
-                if True in matches:
-                    first_match_index = matches.index(True)
-                    name = known_face_names[first_match_index]
-    
-                face_names.append(name)
-    
-        process_this_frame = not process_this_frame
-    
-    
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-    
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-    
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-    
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-    
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
 
-def generateUsers():
-    database = 'employees.db'
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
 
-    # create a database connection
-    conn = sqlite3.connect(database)
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+                markInOffice(conn, name, setInOffice)
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video'+identifier, frame)
+
+def markInOffice(conn, name, inOffice):
+    print('{0} {1} in the office'.format(name, 'is now' if inOffice else 'is no longer'))
+    conn.execute('UPDATE employees SET inOffice = ? where name = ?', (inOffice, name))
+    conn.commit()
+  
+
+def generateUsers(conn):
     conn.row_factory = sqlite3.Row
 
     employee_dict = {}
